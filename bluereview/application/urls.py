d13f809963152fb60c63e900e396db453a36475a
Alex Smith <alexsmith1@me.com>
2019-02-28 14:26:52 -0500

    base application views functioning

--- bluereview/application/urls.py
+++ bluereview/application/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	# path()
+	path('', views.index, name='application-index'),
+	path('index/', views.ApplicationListView.as_view(), name='application-list'),
+	path('<int:pk>', views.ApplicationDetailView.as_view(), name='application-detail')
 ]
--- bluereview/application/views.py
+++ bluereview/application/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from application.models import Application
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	num_apps = Application.objects.all().count()
+	sci_apps = Application.objects.filter(faculty__contains='science').count()
+
+	context = {
+		'num_apps': num_apps,
+		'sci_apps': sci_apps,
+	}
+
+	return render(request, 'index.html', context=context)
 	# return HttpResponse("Hello, world. You're at the application index.")
+
+class ApplicationListView(generic.ListView):
+	model = Application
+		
+class ApplicationDetailView(generic.DetailView):
+	model = Application
